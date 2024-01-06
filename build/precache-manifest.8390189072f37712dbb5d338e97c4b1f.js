137021cf2115efcc811879e913ce31b6a1ad383c
Jon Divi <98039686+jondivi@users.noreply.github.com>
2024-01-06 10:57:13 -0500

    adding build folder

---
+++ build/precache-manifest.8390189072f37712dbb5d338e97c4b1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4108a763426f5f39a3cf7475f72ce277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c241970515391af81075",
+    "url": "/static/css/main.1f5bd49e.chunk.css"
+  },
+  {
+    "revision": "1fe5a3e4906d6bc8d85f",
+    "url": "/static/js/2.dcedfdf3.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.dcedfdf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ceefb0ba066dfaa85d",
+    "url": "/static/js/3.85e73ff5.chunk.js"
+  },
+  {
+    "revision": "c241970515391af81075",
+    "url": "/static/js/main.7afcd20c.chunk.js"
+  },
+  {
+    "revision": "99af3213fa1a10304d61",
+    "url": "/static/js/runtime-main.fe52ca36.js"
+  }
+]);
